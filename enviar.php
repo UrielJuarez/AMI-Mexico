b99de434a4d1d080d5cca8a980ce16d6e2e73992
UrielJuarez <ujjuarezr@gmail.com>
2021-08-06 14:47:46 -0500

    se hace ajuste de campos faltantes en formulario

--- enviar.php
+++ enviar.php
@@ -1,8 +1,11 @@
 <?php
 $name = $_POST['name'];
+$company = $_POST['company'];
 $mail = $_POST['mail'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$city = $_POST['city'];
+
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -10,9 +13,11 @@ $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message = "De la compañia: " . $company . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message = "De la ciudad de : " . $city . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'ujjuarezr@gmail.com';
