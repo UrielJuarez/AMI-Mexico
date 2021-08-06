5d3447a186d9a370bbcfe2f0afdef39f9be7b300
UrielJuarez <ujjuarezr@gmail.com>
2021-08-06 15:16:41 -0500

    cambios

--- enviar.php
+++ enviar.php
@@ -4,20 +4,16 @@ $company = $_POST['company'];
 $mail = $_POST['mail'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$city = $_POST['city'];
-
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message = "De la compañia: " . $company . " \r\n";
+$message = "Este mensaje fue enviado por: " . $name . " \r\n" . " De la compañia: " . $company . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message = "De la ciudad de : " . $city . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'ujjuarezr@gmail.com';
