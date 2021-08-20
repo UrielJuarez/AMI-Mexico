abd8b82fb37fe811288780bba3083b3e5658c731
UrielJuarez <ujjuarezr@gmail.com>
2021-08-19 23:53:29 -0500

    se agrega logica de cargo puesto en php

--- enviar.php
+++ enviar.php
@@ -1,6 +1,7 @@
 <?php
 $name = $_POST['name'];
 $company = $_POST['company'];
+$company = $_POST['cargopuesto'];
 $mail = $_POST['mail'];
 $phone = $_POST['phone'];
 $city = $_POST['city'];
@@ -11,7 +12,7 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n" . "De la compañia: " . $company . " \r\n";
+$message = "Este mensaje fue enviado por: " . $name . " \r\n" . "De la compañia: " . $company . " \r\n" . "Con el cargo o puesto: " . $cargopuesto . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n" . "De la ciudad: " . $city . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
